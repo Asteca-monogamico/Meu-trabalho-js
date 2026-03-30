52ca3cd682d0e9bcb862fcce912e04d78fad7243
Asteca-monogamico <abner.freitas.andrade@escola.pr.gov.br>
2026-03-30 08:17:05 -0300

    Abner_gatão

---
+++ index.js
@@ -0,0 +1,13 @@
+    //Fazendo a soma. 
+    let a = 80;
+    let b = 30;
+
+    //Fazendo a soma e a subtração.
+    let soma = a + b;
+    let sub = a - b;
+
+    //Agora mostrar os resultados para o usuário
+    console.log(`Soma: ${soma}`);
+    console.log(`Subtração: ${sub}`);
+
+    //Condição da soma 
